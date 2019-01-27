3f86956697d97d356c4a72f29e22651f2517a288
fedihor <fedihor@ukr.net>
2019-01-27 16:03:26 +0200

    Init commit

---
+++ SendEmail/EmailService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace SendEmail
+{
+    /// <summary>
+    /// Для того, чтобы отправлять почту через gmail.com нужно после авторизации в аккаунте gmail.com перейти по этой ссылке
+    /// и отметить "Небезопасные приложения разрешены" ("Allow less secure apps"):
+    /// https://www.google.com/settings/security/lesssecureapps
+    /// </summary>
+    public class EmailService
+    {
+        public SmtpClient client = null;
+        public string Host = "smtp.gmail.com";
+        public int Port = 587;
+        public string MailFrom = "MailFrom@gmail.com";
+        public string Password = "Password";
+        public bool IsBodyHtml = true;
+        public List<string> Erors { get; private set; }
+
+        public string MailTo { get; set; }
+        public virtual string Subject { get; set; }
+        public virtual string Body { get; set; }
+
+        public SmtpClient SendBySmtp()
+        {
+            try
+            {
+                // Адрес и порт smtp-сервера, с которого мы и будем отправлять письмо.
+                client = new SmtpClient(Host, Port);
+
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential(MailFrom, Password);
+                client.EnableSsl = true;
+
+                // Создаем письмо.
+                var mail = new MailMessage(MailFrom, MailTo);
+                mail.Subject = Subject;
+                mail.Body = Body;
+                mail.IsBodyHtml = IsBodyHtml;
+
+                // Отправить письмо.
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Erors = new List<string>();
+                Erors.Add(ex.Message);
+                throw new Exception(ex.Message, ex);
+            }
+
+            return client;
+        }
+    }
+}
---
+++ SendEmail/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SendEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailService EmailSender = new EmailService();
+            EmailSender.MailTo = "MailTo@ukr.net";
+            EmailSender.Subject = "Subject";
+            EmailSender.Body = "<h1><font color='red'>Body!!!</font></h1>";
+            EmailSender.SendBySmtp();
+        }
+    }
+}
